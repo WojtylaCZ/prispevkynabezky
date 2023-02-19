e8f44c4b8a16c8aed7e3ac1c2715eef80e9bb269
WojtylaCZ <vojtech.uhlir@productboard.com>
2023-02-19 23:51:00 +0100

    Added react-snap for prerendering to static html for seo

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,43 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Main } from "./app/main";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import SSRProvider from "react-bootstrap/SSRProvider";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>
-);
+import { hydrate, render } from "react-dom";
+
+const rootElement = document.getElementById("root") as HTMLElement;
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <SSRProvider>
+        <Main />
+      </SSRProvider>
+    </React.StrictMode>,
+
+    rootElement
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <SSRProvider>
+        <Main />
+      </SSRProvider>
+    </React.StrictMode>,
+    rootElement
+  );
+}
+
+// import ReactDOM from "react-dom/client";
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <Main />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
